2c5e3d77bb1f5f393f34c16daa7d742c5a020232
Դավիթ Գրիգորյան <69806082+grigoryan69@users.noreply.github.com>
2022-03-24 00:17:06 +0400

    Update recusion.py

--- recusion.py
+++ recusion.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python3
-# recursion function to print from number to 1
-def from_num_to_one(num) -> int:
-	print(int(num))
-	if int(num) == 1:
-		return 1
-	else:
-		return from_num_to_one(int(num) - 1)
-
-
-# same recursion function to print from number to 1 but shorter
-def from_num_to_one_short(num) -> int:
-	print(int(num))
-	return 1 if int(num) == 1 else from_num_to_one_short(int(num) - 1)
-
-
 # recursion function to print from 1 to number
 def from_one_to_num(num) -> int:
 	if int(num) == 0:
@@ -21,15 +6,11 @@ def from_one_to_num(num) -> int:
 	else:
 		from_one_to_num(int(num) - 1)
 	print(int(num))
-
+	
 
 def main():
-	from_num_to_one(100)
-	from_num_to_one_short(100)
 	from_one_to_num(100)
-
+	
 
 if __name__ == '__main__':
 	main()
-
-
